a323a8a982d49764be5e876e5f22141b7bc1e6a8
TeodorDre <swen295@gmail.com>
2020-04-04 03:58:55 +0300

    feat(core) - add write json file

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
+from pathlib import Path
+import os
+
+
+class JSONStore:
+    def __init__(self, path):
+        self.path = path
+
+
 class BaseServer:
     def __init__(self):
         self.port = 3330
         self.store = None
 
     def init_services(self):
-        print('init Services')
+        current_file_folder = __file__
+
+        store_path = Path(current_file_folder, '..', 'server', 'storage', 'store.json').resolve()
+
+        try:
+            open(store_path, 'r')
+        except FileNotFoundError as error:
+            if error.errno == 2:
+                os.makedirs(Path(current_file_folder, '..', 'server', 'storage').resolve(), exist_ok=True)
+                open(store_path, 'w')
+
+        self.store = JSONStore(store_path)
 
 
 server = BaseServer()
