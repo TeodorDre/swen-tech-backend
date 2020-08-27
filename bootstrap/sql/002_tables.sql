d26cbd51c6b0dbbabf713e751ac8ceb43723ab42
TeodorDre <swen295@gmail.com>
2020-08-27 17:34:18 +0300

    fix(core): update tables

--- bootstrap/sql/002_tables.sql
+++ bootstrap/sql/002_tables.sql
@@ -21,51 +21,50 @@ CREATE TABLE if not exists swentech.sessions (
   CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
 
-CREATE TABLE if not exists swentech.projects (
-  project_id          SERIAL PRIMARY KEY,
-  project_name        TEXT        NOT NULL unique,
-  project_description TEXT,
-  created_ts          TIMESTAMPTZ NOT NULL default now(),
-  updated_ts          TIMESTAMPTZ NOT NULL default now()
+CREATE TABLE if not exists swentech.categories (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
 
-CREATE TABLE if not exists swentech.datasets (
-  dataset_id          SERIAL PRIMARY KEY,
-  dataset_name        TEXT        NOT NULL,
-  dataset_description TEXT,
-  project_id          bigint      not null references swentech.projects ON DELETE CASCADE,
-  created_ts          TIMESTAMPTZ NOT NULL default now(),
-  updated_ts          TIMESTAMPTZ NOT NULL default now(),
-  CONSTRAINT DS_PROJ UNIQUE (dataset_name, project_id)
+CREATE TABLE if not exists swentech.categories_lang (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
 
-CREATE TABLE if not exists swentech.images (
-  image_id          BIGSERIAL PRIMARY KEY,
-  image_name        TEXT        NOT NULL,
-  image_description TEXT,
-  image_link        TEXT        not null,
-  width             INTEGER     NOT NULL,
-  height            INTEGER     NOT NULL,
-  dataset_id        BIGINT      not null references swentech.datasets ON DELETE CASCADE,
-  created_ts        TIMESTAMPTZ NOT NULL default now(),
-  updated_ts        TIMESTAMPTZ NOT NULL default now(),
-  CONSTRAINT IM_DS UNIQUE (image_name, dataset_id),
-  CONSTRAINT LINK_DS UNIQUE (image_link, dataset_id)
+CREATE TABLE if not exists swentech.tags (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
 
-CREATE TABLE if not exists swentech.labels (
-  label_id          SERIAL PRIMARY KEY,
-  label_name        TEXT        NOT NULL unique,
-  label_description TEXT,
-  color             TEXT        not null,
-  parameters        JSONB,
-  created_ts        TIMESTAMPTZ NOT NULL default now(),
-  updated_ts        TIMESTAMPTZ NOT NULL default now()
+CREATE TABLE if not exists swentech.tags_lang (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
 
-CREATE TABLE if not exists swentech.annotations (
-  image_id    BIGINT      not null unique references swentech.images ON DELETE CASCADE,
-  annotations JSONB       not null,
-  created_ts  TIMESTAMPTZ NOT NULL default now(),
-  updated_ts  TIMESTAMPTZ NOT NULL default now()
+CREATE TABLE if not exists swentech.posts (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
+);
+
+CREATE TABLE if not exists swentech.posts_lang (
+  client_id     SERIAL      not null unique references swentech.users ON DELETE CASCADE,
+  session_id    TEXT        NOT NULL unique,
+
+  created_ts    TIMESTAMPTZ NOT NULL default now(),
+  CONSTRAINT CL_SESS UNIQUE (client_id, session_id)
 );
