8a9cee8ef19681b51e9a772bd64b11d2b0af6c4d
TeodorDre <swen295@gmail.com>
2020-11-29 18:23:43 +0300

    feat(app): add version to app

--- app/platform/router/handlers/echo_handler.py
+++ app/platform/router/handlers/echo_handler.py
@@ -2,6 +2,7 @@ from app.platform.router.router_handler import RouteHandler
 from app.platform.log.log_service import LogService
 from aiohttp import web, hdrs
 from app.platform.router.common import send_success_response
+from configuration import APP
 
 
 class EchoRouteHandler(RouteHandler):
@@ -16,4 +17,7 @@ class EchoRouteHandler(RouteHandler):
         self.name = 'common.echo'
 
     def handler(self, request: web.Request) -> web.Response:
-        return send_success_response(self.name, 'OK')
+        return send_success_response(self.name, {
+            'result': 'OK',
+            'appVersion': APP['version']
+        })
